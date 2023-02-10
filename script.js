b6b91539ec7dd0082c3b9dae91a71a25a56728e2
Zer0cheros <christian.wiss@optimaedu.fi>
2023-02-10 08:44:25 +0200

    test

---
+++ script.js
@@ -0,0 +1,27 @@
+function loadJSON(path, success, error)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    success(JSON.parse(xhr.responseText));
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
+
+
+loadJSON('./energy.json' ,
+        (data)=> {
+            //här fortsätter du
+         console.log(data);
+        },
+        (err)=> { console.error(err); }
+);
