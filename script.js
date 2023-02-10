c0dc8da36d3f87dabe9e5366ad9759eb2d166592
Zer0cheros <christian.wiss@optimaedu.fi>
2023-02-10 12:29:46 +0200

    final

--- script.js
+++ script.js
@@ -17,16 +17,28 @@ function loadJSON(path, success, error)
     xhr.send();
 }
 
-
+const root = document.querySelector('.root')
 loadJSON('./energy.json' ,
         (data)=> {
             //här fortsätter du
-            //displayData(data)
-         console.log(data);
+            displayData(data)
         },
         (err)=> { console.error(err); }
 );
 
 function displayData(json){
-
+    root.innerHTML = `
+    <tr>
+        <th>Country</th>
+        <th>Consumption</th>
+        <th>Flag</th>
+    </tr>
+    ${json.map(d=> root.innerHTML = `
+        <tr>
+            <td>${d.country}</td>
+            <td>${d.consumption}</td>
+            <td><img height="50px" width="100px" src="${d.flag}" /></td>
+        </tr>
+    `)}
+    `
 }
