41bd7049561bc55337849c8f9ed065fa4e467c80
FilipBjorkskog <filip.bjorkskog@gmail.com>
2023-02-10 12:31:53 +0200

    Innit

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-function loadJSON(path, success, error)
-{
+function loadJSON(path, success, error) {
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function()
-    {
+    xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 if (success)
@@ -18,10 +16,18 @@ function loadJSON(path, success, error)
 }
 
 
-loadJSON('./energy.json' ,
-        (data)=> {
-            //här fortsätter du
-         console.log(data);
-        },
-        (err)=> { console.error(err); }
+loadJSON('./energy.json',
+    (data) => {
+        let table = "<body><h2>HTML Table</h2><table><tr><th>Flag</th><th>Country</th><th>Consumption</th></tr>";
+
+        for (let i = 0; i < data.length; i++) {
+            table += "<tr><td><img src='" + data[i].flag + "' width='30' height='20'/></td><td>" + data[i].country + "</td><td>" + data[i].consumption + "</td></tr>";
+        }
+
+        table += "</table></body>";
+
+        document.body.innerHTML = table;
+        console.log(data);
+    },
+    (err) => { console.error(err); }
 );
